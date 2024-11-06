40e32e77a833b5765785161f06f1ab5e3da69e18
Yash Agarwal <190e785@gmail.com>
2024-11-06 14:16:41 +0530

    Update vite.config.js
    Update deploy.yml to add event triggers and settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/190-785/First_Link'  // Update with your actual repo name
 })
