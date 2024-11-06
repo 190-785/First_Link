e9c01cc3af8dfd441bc9aec72e6da2762e931d39
Yash Agarwal <190e785@gmail.com>
2024-11-06 14:42:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/190-785/First_Link' 
+  base: 'https://github.com/190-785/First_Link/' 
 })
